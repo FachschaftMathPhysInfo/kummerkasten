bd168182310e37d922630a87bc0e469cf9c9f72a
Henrik Reinstädtler <henrixapp@hotmail.com>
2017-08-22 20:55:05 +0200

    Begin Adminoberfläche

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_ember_app :admin, to: "/admin/"
   mount_ember_app :frontend, to: "/"
   devise_for :lecturers, controllers: { sessions: 'sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170822153431) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,11 +88,11 @@ ActiveRecord::Schema.define(version: 20170822153431) do
     t.datetime "updated_at", null: false
     t.string "authentication_token", limit: 30
     t.string "notifications", default: "every"
+    t.integer "lsf_id"
+    t.string "password"
     t.index ["authentication_token"], name: "index_lecturers_on_authentication_token", unique: true
     t.index ["email"], name: "index_lecturers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_lecturers_on_reset_password_token", unique: true
-    t.integer "lsf_id"
-    t.string "password"
   end
 
   create_table "lectures", force: :cascade do |t|
