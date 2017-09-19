cab192ad31f44a4864d84f64c29e72a05bea292a
Henrik Reinstädtler <henrixapp@hotmail.com>
2017-09-19 16:20:27 +0200

    Hinzufügen einer grundlegenden Dockerfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => ENV["SMTP_ADDRESS"], :port => ENV["SMTP_PORT"] }
 end
