79dcbedb2001b4ead711bfb0994f50950b51dda4
Henrik Reinstädtler <henrixapp@hotmail.com>
2017-09-27 14:45:09 +0200

    Fehler in has-read

--- admin/app/helpers/has-read.js
+++ admin/app/helpers/has-read.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export function hasRead(params/*, hash*/) {
+  return new Ember.RSVP.Promise((fullfill,reject)=>{
   params[0].get('readers').then((list)=>{
-    return list.includes(params[1]);
-  });
+    fullfill( list.includes(params[1]));
+  });});
 }
 
 export default Ember.Helper.helper(hasRead);
