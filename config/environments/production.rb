2008719bfa3733b9b5b079d34e774afc80f47142
Henrik Reinstädtler <henrixapp@hotmail.com>
2017-10-16 17:04:15 +0200

    Fixing allowed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
    config.action_cable.url = 'wss://kummerkasten.mathphys.stura.uni-heidelberg.de/cable'
-   config.action_cable.allowed_request_origins = [ 'http://example.com', /https:\/\/*/ ]
+   config.action_cable.allowed_request_origins = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
