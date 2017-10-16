a3094b860a6a4ecb58c23e53aebc0667acc86e46
Henrik Reinstädtler <henrixapp@hotmail.com>
2017-10-16 16:49:57 +0200

    Action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.url = 'wss://kummerkasten.mathphys.stura.uni-heidelberg.de/cable'
+   config.action_cable.allowed_request_origins = [ 'http://example.com', /https:\/\/*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
