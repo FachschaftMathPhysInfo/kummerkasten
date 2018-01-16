5e165a3cc3b9251c6f63b12d314c697d3d72e5a1
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-01-16 15:50:28 +0100

    Dummer Fehler in Reminder

--- app/mailers/remind_complaint_mailer.rb
+++ app/mailers/remind_complaint_mailer.rb
@@ -2,8 +2,8 @@ class RemindComplaintMailer < ApplicationMailer
   default from: 'notifications@mathphys.stura.uni-heidelberg.de'
 
 def unchecked_email()
-  @comment_count = Complaint.where("created_at < ?", 7.days.ago).count
-  @not_processed = Complaint.where("created_at < ? AND reviewed=?", 7.days.ago,false).count
+  @comment_count = Complaint.where("created_at > ?", 7.days.ago).count
+  @not_processed = Complaint.where("created_at > ? AND reviewed=?", 7.days.ago,false).count
   mail(to: "fachschaft@mathphys.stura.uni-heidelberg.de", subject: "[Kummerkasten] #{@comment_count} neue, nichtfreigeschaltete Kummernachrichten")
 end
 end
