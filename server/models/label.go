2bab2e239d566763b43e18a8f6064135e342628a
calvalre <olga.sergeyeva@outlook.de>
2025-07-15 17:34:32 +0200

    fix label and tickets schema

--- server/models/label.go
+++ server/models/label.go
@@ -7,8 +7,8 @@ import (
 type Label struct {
 	bun.BaseModel `bun:"table:labels"`
 
-	ID      string  `bun:",pk,default:gen_random_UUID(),type:uuid"`
-	Name    string  `bun:",notnull"`
-	Color   string  `bun:"type:varchar(8),default:'#7a7777'"`
-	Tickets []Label `bun:",notnull"`
+	ID      string    `bun:",pk,default:gen_random_UUID(),type:uuid"`
+	Name    string    `bun:",notnull"`
+	Color   string    `bun:"type:varchar(8),default:'#7a7777'"`
+	Tickets []*Ticket `bun:"m2m:labels_tickets,join:Ticket=Label"`
 }
