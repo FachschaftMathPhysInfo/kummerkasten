55164beb8774b0d8077f4dd798a878e31ddd22c2
calvalre <olga.sergeyeva@outlook.de>
2025-07-19 00:43:05 +0200

    fix warnings

--- server/models/label.go
+++ server/models/label.go
@@ -8,5 +8,5 @@ type Label struct {
 	ID      string    `bun:",pk,default:gen_random_UUID(),type:uuid"`
 	Name    string    `bun:",notnull"`
 	Color   string    `bun:"type:varchar(8),default:'#7a7777'"`
-	Tickets []*Ticket `bun:"m2m:labels_to_tickets,label_id,ticket_id"`
+	Tickets []*Ticket `bun:"m2m:labels_to_tickets"`
 }
