ff4e82d510ea0235b85e99a1c3171bfe928c607a
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-07-18 22:50:35 +0200

    implement: only-self directive

--- server/graph/directives/directives.go
+++ server/graph/directives/directives.go
@@ -21,3 +21,27 @@ func HasRole(ctx context.Context, obj interface{}, next graphql.Resolver, role *
 	}
 	return nil, fmt.Errorf("access denied")
 }
+
+func OnlySelf(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
+	user, ok := ctx.Value(middleware.UserKey).(*model.User)
+
+	if user == nil || !ok {
+		return nil, fmt.Errorf("access denied")
+	}
+
+	if user.Role == model.UserRoleAdmin {
+		return next(ctx)
+	}
+
+	fieldCtx := graphql.GetFieldContext(ctx)
+	if fieldCtx == nil {
+		return nil, fmt.Errorf("access denied")
+	}
+	args := graphql.GetFieldContext(ctx).Args
+
+	if id, exists := args["id"]; exists && id == user.ID {
+		return next(ctx)
+	}
+
+	return nil, fmt.Errorf("denied: can only update self")
+}
