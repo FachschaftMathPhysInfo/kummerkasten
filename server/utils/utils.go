0cfedc8f40dc9bf2bdaa4295742ad89a16e3b155
calvalre <olga.sergeyeva@outlook.de>
2025-07-19 10:53:35 +0200

    fix linter error

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -10,7 +10,7 @@ func RandString(entropy int) (string, error) {
 	b := make([]byte, entropy)
 	_, err := rand.Read(b)
 	if err != nil {
-		return "", fmt.Errorf("error generating random string:", err)
+		return "", fmt.Errorf("error generating random string %w", err)
 	}
 	return base64.RawStdEncoding.EncodeToString(b), nil
 }
