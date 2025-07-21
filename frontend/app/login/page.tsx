c31c3e6bbf9b5bc87349d53f62d3f4734e4d4f59
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-07-21 07:27:19 +0200

    fix: unused imports

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 
-import {useUser} from "@/components/providers/user-provider";
 import {Card, CardContent, CardTitle} from "@/components/ui/card";
 import LoginForm from "@/app/login/login-form";
-import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export default function LoginPage() {
   const [loading, setLoading] = useState<boolean>(false);
