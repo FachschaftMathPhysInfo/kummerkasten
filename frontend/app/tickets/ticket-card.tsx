ef5164138fa4b89b90a40ec6ef975b9b736a677f
calvalre <olga.sergeyeva@outlook.de>
2025-08-10 18:51:55 +0200

    update design

--- frontend/app/tickets/ticket-card.tsx
+++ frontend/app/tickets/ticket-card.tsx
@@ -1,9 +1,8 @@
 "use client"
 
 import React, {useCallback, useEffect, useState} from "react";
-import {Card, CardContent, CardTitle} from "@/components/ui/card";
+import {Card, CardTitle} from "@/components/ui/card";
 import {Label, Ticket, TicketByIdDocument, TicketByIdQuery} from "@/lib/graph/generated/graphql";
-import {SeperatorHorizontal} from "@/components/seperator-horizontal";
 import {Link, MoreHorizontal, MoreVertical, NotepadText, Trash2} from "lucide-react";
 import {Badge} from "@/components/ui/badge"
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu";
@@ -47,11 +46,11 @@ export function TicketCard({ticketID}: TicketProps) {
     }, [fetchTicketData]);
 
     return (
-        <Card className="w-full">
-            <CardTitle className="flex ml-4 justify-between">
-                <div className="flex items-center">
+        <Card className="w-full p-3">
+            <CardTitle className="flex flex-col ml-2 justify-between">
+                <div className="flex justify-between items-center w-full">
                     <Badge
-                        className="mr-2 text-white"
+                        className="text-white"
                         style={{
                             backgroundColor:
                                 ticket?.state === "NEW"
@@ -72,49 +71,48 @@ export function TicketCard({ticketID}: TicketProps) {
                     <div className="flex text-md absolute ml-[60px]">
                         {ticket?.title}
                     </div>
-                </div>
+                    <div className="flex flex-col items-end">
+                        <div className=" md:flex md:mr-1">
+                            {ticketLabels?.map((label) => (
+                                label?.id &&
+                                <Badge key={label.id} className="hidden md:flex md:mx-1  justify-center text-white"
+                                       style={{backgroundColor: label.color ?? "#000000"}}>{label.name}</Badge>
+                            ))}
+                            <div
+                                className="hidden mx-3 md:flex flex-col text-xs items-end justify-center text-muted-foreground">
+                                Geändert: {new Date(ticket?.lastModified || '').toLocaleDateString()}
+                            </div>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <div className="cursor-pointer flex items-center justify-center">
+                                        {isMobile ? (
+                                            <MoreHorizontal className="w-6 h-6"/>
+                                        ) : (
+                                            <MoreVertical className="w-6 h-6"/>
+                                        )}
+                                    </div>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent side="bottom" align="end">
+                                    <DropdownMenuItem onClick={() => console.log("Edit", ticketID)}>
+                                        <NotepadText/> Bearbeiten
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => console.log("Link", ticketID)}>
+                                        <Link/> Link kopieren
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem
+                                        onClick={() => console.log("Delete", ticketID)}
+                                        className="text-destructive"
+                                    >
+                                        <Trash2 className="text-destructive"/> Löschen
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
 
-                <div className="flex flex-col items-end">
-                    <div className="hidden md:flex md:mr-1">
-                        {ticketLabels?.map((label) => (
-                            label?.id && <Badge key={label.id} className="md:mx-1 text-white"
-                                                style={{backgroundColor: label.color ?? "#000000"}}>{label.name}</Badge>
-                        ))}
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <div className="cursor-pointer flex justify-center">
-                                    {isMobile ? (
-                                        <MoreHorizontal className="w-6 h-6"/>
-                                    ) : (
-                                        <MoreVertical className="w-6 h-6"/>
-                                    )}
-                                </div>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent side="bottom" align="end">
-                                <DropdownMenuItem onClick={() => console.log("Edit", ticketID)}>
-                                    <NotepadText/> Bearbeiten
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => console.log("Link", ticketID)}>
-                                    <Link/> Link kopieren
-                                </DropdownMenuItem>
-                                <DropdownMenuItem
-                                    onClick={() => console.log("Delete", ticketID)}
-                                    className="text-destructive"
-                                >
-                                    <Trash2 className="text-destructive"/> Löschen
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
                     </div>
                 </div>
-            </CardTitle>
-            <SeperatorHorizontal className="hidden md:flex mx-4"/>
-            <CardContent className="flex">
-                <div>
-                    <div className="bold text-md">{ticket?.text}</div>
-                </div>
-            </CardContent>
 
+            </CardTitle>
         </Card>
 
     )
