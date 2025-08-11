ad7da9ab70a530f967e765a7b93845b79d2e7cd8
calvalre <olga.sergeyeva@outlook.de>
2025-08-11 16:06:04 +0200

    add newest ticket overview design

--- frontend/app/tickets/[ticketId]/page.tsx
+++ frontend/app/tickets/[ticketId]/page.tsx
@@ -9,11 +9,13 @@ import {getClient} from "@/lib/graph/client";
 import {
     AllTicketsDocument,
     AllTicketsQuery,
+    Label,
     Ticket,
     TicketByIdDocument,
     TicketByIdQuery
 } from "@/lib/graph/generated/graphql";
-import {Card, CardContent} from "@/components/ui/card";
+import {Link, NotepadText, Trash2, X} from "lucide-react";
+import {Button} from "@/components/ui/button";
 
 
 const client = getClient();
@@ -27,6 +29,7 @@ export default function TicketDetailView() {
 
     const [ticket, setTicket] = useState<Ticket | null>(null);
     const ticketId = params.ticketId;
+    const [ticketLabels, setTicketLabels] = useState<Label[]>([]);
 
     const fetchTickets = useCallback(async () => {
         const data = await client.request<AllTicketsQuery>(AllTicketsDocument);
@@ -35,7 +38,12 @@ export default function TicketDetailView() {
 
     const fetchTicketDetail = useCallback(async () => {
         const data = await client.request<TicketByIdQuery>(TicketByIdDocument, {id: ticketId});
-        if (data.tickets?.[0]) setTicket(data.tickets[0]);
+        const ticketData = data?.tickets?.[0];
+        const labels = ticketData?.labels;
+        if (ticketData) {
+            setTicket(ticketData);
+            setTicketLabels(labels ?? []);
+        }
     }, [ticketId]);
 
     useEffect(() => {
@@ -53,7 +61,7 @@ export default function TicketDetailView() {
     return (
         <div className="w-full h-full flex flex-col pt-2 grow">
             <ResizablePanelGroup direction="horizontal" className="flex flex-grow">
-                <ResizablePanel defaultSize={30}
+                <ResizablePanel defaultSize={30} minSize={20} maxSize={30}
                                 className="border-r border-gray-500 flex flex-col">
                     <div className="px-4">
                         <Input
@@ -62,15 +70,15 @@ export default function TicketDetailView() {
                             onChange={e => setSearchTerm(e.target.value)}
                             className="mb-4"
                         />
-                        <div className="flex-row">
+                        <div className="">
                             {filteredTickets.map(t => (
                                 <div
                                     key={t.id}
-                                    className={`p-2 cursor-pointer rounded ${t.id === ticketId ? "bg-background" : "hover:bg-accent"}`}
+                                    className={`p-2 cursor-pointer rounded ${t.id === ticketId ? "bg-accent" : "hover:bg-accent"}`}
                                     onClick={() => router.push(`/tickets/${t.id}`)}
                                 >
                                     <Badge
-                                        className="text-white px-2 py-1 rounded mr-2"
+                                        className="text-white px-2 py-1 rounded mr-5"
                                         style={{
                                             backgroundColor:
                                                 t.state === "NEW" ? "#839176" :
@@ -78,7 +86,6 @@ export default function TicketDetailView() {
                                                         t.state === "CLOSED" ? "#DF517F" : "gray"
                                         }}
                                     >
-                                        {t.state.toLowerCase()}
                                     </Badge>
                                     {t.title}
                                 </div>
@@ -91,7 +98,7 @@ export default function TicketDetailView() {
                                 className="border-r border-gray-500 flex flex-col">
                     {ticketId ? (
                         ticket ? (
-                            <div className="flex mx-3">
+                            <div className="flex mx-6">
                                 <div className="flex flex-col">
                                     <h1 className="text-2xl font-semibold mb-4">{ticket.title}</h1>
                                     <div>{ticket.text}</div>
@@ -107,37 +114,65 @@ export default function TicketDetailView() {
                         </div>
                     )}
                 </ResizablePanel>
-                <ResizablePanel defaultSize={15}>
+                <ResizablePanel defaultSize={13}>
                     {ticket ? (
-                        <div className="flex justify-center">
-                            <Card className="flex w-[80%] border-dotted border-2 px-0">
-                                <CardContent className="flex flex-col text-sm">
-                                    <div className="flex justify-between items-center">
-                                        <div>
-                                            Status:
-                                        </div>
-                                        <Badge
-                                            className="text-white rounded"
-                                            style={{
-                                                backgroundColor:
-                                                    ticket.state === "NEW" ? "#839176" :
-                                                        ticket.state === "OPEN" ? "#192B51" :
-                                                            ticket.state === "CLOSED" ? "#DF517F" : "gray"
-                                            }}
+                        <div className="flex flex-col justify-between items-center h-full">
+                            <div className="flex items-stretch justify-center">
+                                <Button variant="ghost">
+                                    <Link/>
+                                </Button>
+                                <Button variant="ghost">
+                                    <NotepadText/>
+                                </Button>
+                                <Button variant="ghost">
+                                    <Trash2/>
+                                </Button>
+                                <Button variant="ghost">
+                                    <X/>
+                                </Button>
+                            </div>
+                            <div
+                                className="flex flex-col border-2 border-dotted rounded-2xl w-[70%] justify-center p-2">
+                                <div className="flex justify-between items-center">
+                                    <div className="text-sm ">
+                                        Status:
+                                    </div>
+                                    <Badge
+                                        className="text-white rounded"
+                                        style={{
+                                            backgroundColor:
+                                                ticket.state === "NEW" ? "#839176" :
+                                                    ticket.state === "OPEN" ? "#192B51" :
+                                                        ticket.state === "CLOSED" ? "#DF517F" : "gray"
+                                        }}
+                                    >
+                                        {ticket.state.toLowerCase()}
+                                    </Badge>
+                                </div>
+                                <div className="flex text-sm justify-between items-center">
+                                    <div>Erstellt:</div>
+                                    <div>{new Date(ticket.createdAt).toLocaleDateString()}</div>
+                                </div>
+                                <div className="flex text-sm justify-between items-center">
+                                    <div>Geändert:</div>
+                                    <div>{new Date(ticket.lastModified).toLocaleDateString()}</div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col">
+                                {ticketLabels?.map((label) => (
+                                    label?.id &&
+                                    <Badge key={label.id}
+                                           className="hidden md:flex mb-3 w-full text-white justify-between"
+                                           style={{backgroundColor: label.color ?? "#000000"}}>{label.name}
+                                        <button
+                                            type="button"
+                                            className="p-0 m-0 hover:opacity-75"
                                         >
-                                            {ticket.state.toLowerCase()}
-                                        </Badge>
-                                    </div>
-                                    <div className="flex text-sm justify-between items-center">
-                                        <div>Erstellt:</div>
-                                        <div>{new Date(ticket.createdAt).toLocaleDateString()}</div>
-                                    </div>
-                                    <div className="flex text-sm justify-between items-center">
-                                        <div>Geändert:</div>
-                                        <div>{new Date(ticket.lastModified).toLocaleDateString()}</div>
-                                    </div>
-                                </CardContent>
-                            </Card>
+                                            <X className="h-3 w-3"/>
+                                        </button>
+                                    </Badge>
+                                ))}
+                            </div>
                         </div>) : (
                         <p>Lade Daten ...</p>
                     )}
