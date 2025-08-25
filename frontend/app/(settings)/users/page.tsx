e60ed6f8441378b964523d6b33aa3d5391243b94
calvalre <olga.sergeyeva@outlook.de>
2025-08-26 00:08:56 +0200

    change page headers to component

--- frontend/app/(settings)/users/page.tsx
+++ frontend/app/(settings)/users/page.tsx
@@ -6,44 +6,41 @@ import {AllUsersDocument, AllUsersQuery} from "@/lib/graph/generated/graphql"
 import {getClient} from "@/lib/graph/client";
 import {toast} from "sonner";
 import {Users} from "lucide-react";
+import {ManagementPageHeader} from "@/components/management-page-header";
 
 export default function UserManagementPage() {
-  const [users, setUsers] = useState<TableUser[]>([])
+    const [users, setUsers] = useState<TableUser[]>([])
 
-  const fetchUsers = useCallback(async () => {
-    const client = getClient();
+    const fetchUsers = useCallback(async () => {
+        const client = getClient();
 
-    try {
-      const data = await client.request<AllUsersQuery>(AllUsersDocument)
+        try {
+            const data = await client.request<AllUsersQuery>(AllUsersDocument)
 
-      if (!data.users) {
-        setUsers([])
-        return
-      }
+            if (!data.users) {
+                setUsers([])
+                return
+            }
 
-      setUsers(data.users.filter(user => !!user));
-    } catch (error) {
-      toast.error("Fehler beim Laden der User")
-      console.error(error)
-    }
-  }, [])
+            setUsers(data.users.filter(user => !!user));
+        } catch (error) {
+            toast.error("Fehler beim Laden der User")
+            console.error(error)
+        }
+    }, [])
 
-  useEffect(() => void fetchUsers(), [fetchUsers])
+    useEffect(() => void fetchUsers(), [fetchUsers])
 
-  return (
-    <div className={'w-full h-full flex flex-col gap-6 px-10 grow'}>
-      {/*TODO: replace this with component when available*/}
-      <div className={'flex flex-col'}>
-        <span className={'flex gap-2 items-center'}>
-          <Users/>
-          <h1 className={'text-2xl font-bold'}>User Verwaltung</h1>
-        </span>
-
-        <p className={'text-xl text-muted-foreground'}>
-          Verwalte hier User und deren Rollen
-        </p>
-      </div>
-      <UserTable data={users} refreshData={fetchUsers}/>
-    </div>
-  )
+    return (
+        <div className="w-full h-full flex flex-col grow">
+            <ManagementPageHeader
+                icon={<Users/>}
+                title="User Verwaltung"
+                description="Verwalte hier User und deren Rollen."
+            />
+            <div className={'w-full h-full flex flex-col gap-6 px-10 pt-4 grow'}>
+                <UserTable data={users} refreshData={fetchUsers}/>
+            </div>
+        </div>
+    )
 }
