1f8c6741fd178c700cd8291062e2cfc73cd61b55
calvalre <olga.sergeyeva@outlook.de>
2025-08-27 01:42:12 +0200

    fix spacing

--- frontend/app/(settings)/labels/page.tsx
+++ frontend/app/(settings)/labels/page.tsx
@@ -9,37 +9,37 @@ import {LabelTable} from "@/app/(settings)/labels/label-table";
 import {ManagementPageHeader} from "@/components/management-page-header";
 
 export default function LabelManagementPage() {
-    const [labels, setLabels] = useState<Label[]>([])
+  const [labels, setLabels] = useState<Label[]>([])
 
-    const fetchLabels = useCallback(async () => {
-        const client = getClient();
+  const fetchLabels = useCallback(async () => {
+    const client = getClient();
 
-        try {
-            const data = await client.request<AllLabelsQuery>(AllLabelsDocument)
+    try {
+      const data = await client.request<AllLabelsQuery>(AllLabelsDocument)
 
-            if (!data.labels) {
-                setLabels([])
-                return
-            }
+      if (!data.labels) {
+        setLabels([])
+        return
+      }
 
-            setLabels(data.labels.filter(label => !!label));
-        } catch {
-            toast.error("Fehler beim Laden der Label")
-        }
-    }, [])
+      setLabels(data.labels.filter(label => !!label));
+    } catch {
+      toast.error("Fehler beim Laden der Label")
+    }
+  }, [])
 
-    useEffect(() => void fetchLabels(), [fetchLabels])
+  useEffect(() => void fetchLabels(), [fetchLabels])
 
-    return (
-        <div className="w-full h-full flex flex-col grow">
-            <ManagementPageHeader
-                icon={<Tags/>}
-                title="Label Verwaltung"
-                description="Erstelle, lösche und update Labels hier."
-            />
-            <div className={'w-full h-full flex flex-col gap-6 px-10 pt-4 grow'}>
-                <LabelTable data={labels} refreshData={fetchLabels}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full h-full flex flex-col grow">
+      <ManagementPageHeader
+        icon={<Tags/>}
+        title="Label Verwaltung"
+        description="Erstelle, lösche und update Labels hier."
+      />
+      <div className={'w-full h-full flex flex-col gap-6 px-10 pt-4 grow'}>
+        <LabelTable data={labels} refreshData={fetchLabels}/>
+      </div>
+    </div>
+  )
 }
--- frontend/app/(settings)/users/page.tsx
+++ frontend/app/(settings)/users/page.tsx
@@ -9,38 +9,38 @@ import {Users} from "lucide-react";
 import {ManagementPageHeader} from "@/components/management-page-header";
 
 export default function UserManagementPage() {
-    const [users, setUsers] = useState<TableUser[]>([])
+  const [users, setUsers] = useState<TableUser[]>([])
 
-    const fetchUsers = useCallback(async () => {
-        const client = getClient();
+  const fetchUsers = useCallback(async () => {
+    const client = getClient();
 
-        try {
-            const data = await client.request<AllUsersQuery>(AllUsersDocument)
+    try {
+      const data = await client.request<AllUsersQuery>(AllUsersDocument)
 
-            if (!data.users) {
-                setUsers([])
-                return
-            }
+      if (!data.users) {
+        setUsers([])
+        return
+      }
 
-            setUsers(data.users.filter(user => !!user));
-        } catch (error) {
-            toast.error("Fehler beim Laden der User")
-            console.error(error)
-        }
-    }, [])
+      setUsers(data.users.filter(user => !!user));
+    } catch (error) {
+      toast.error("Fehler beim Laden der User")
+      console.error(error)
+    }
+  }, [])
 
-    useEffect(() => void fetchUsers(), [fetchUsers])
+  useEffect(() => void fetchUsers(), [fetchUsers])
 
-    return (
-        <div className="w-full h-full flex flex-col grow">
-            <ManagementPageHeader
-                icon={<Users/>}
-                title="User Verwaltung"
-                description="Verwalte hier User und deren Rollen."
-            />
-            <div className={'w-full h-full flex flex-col gap-6 px-10 pt-4 grow'}>
-                <UserTable data={users} refreshData={fetchUsers}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full h-full flex flex-col grow">
+      <ManagementPageHeader
+        icon={<Users/>}
+        title="User Verwaltung"
+        description="Verwalte hier User und deren Rollen."
+      />
+      <div className={'w-full h-full flex flex-col gap-6 px-10 pt-4 grow'}>
+        <UserTable data={users} refreshData={fetchUsers}/>
+      </div>
+    </div>
+  )
 }
