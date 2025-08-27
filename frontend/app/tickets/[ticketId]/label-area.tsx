d8d289825f8a17340095119531e125947dd37835
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-08-27 16:53:20 +0200

    fix: badge colors

--- frontend/app/tickets/[ticketId]/label-area.tsx
+++ frontend/app/tickets/[ticketId]/label-area.tsx
@@ -1,6 +1,7 @@
 import {Label} from "@/lib/graph/generated/graphql";
 import {Badge} from "@/components/ui/badge";
 import React from "react";
+import {calculateFontColor} from "@/lib/calculate-colors";
 
 interface LabelAreaProps {
   labels: Label[]
@@ -12,14 +13,13 @@ export default function LabelArea({labels}: LabelAreaProps) {
       {labels.map((label) => (
         <Badge
           key={label.id}
-          className="text-white max-w-full px-1"
-          style={{backgroundColor: label.color}}
+          className="max-w-full px-1"
+          style={{backgroundColor: label.color, color: calculateFontColor(label.color)}}
           data-cy={`ticket-label-${label.id}`}
         >
           <span className="truncate max-w-full px-1">{label.name}</span>
         </Badge>
       ))}
     </div>
-
   )
 }
