1aaa71df62c269e2b0236a106f10e48fd54a0b4d
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-09-10 17:08:41 +0200

    fix: olga spacing

--- frontend/app/(settings)/app-settings/page.tsx
+++ frontend/app/(settings)/app-settings/page.tsx
@@ -4,14 +4,14 @@ import FooterForm from "@/app/(settings)/app-settings/footer-form";
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex-col justify-between grow">
+    <div className="w-full h-full flex-col grow">
       <ManagementPageHeader
         icon={<SlidersVertical/>}
         title="App Einstellungen"
         description="Bearbeite hier generelle App Einstellungen"
       />
 
-      <div className={'w-full flex flex-col grow gap-y-6 items-center p-4 md:p-12 pt-0 mt-10'}>
+      <div className={'w-full flex flex-col grow gap-y-6 items-center px-4 md:px-10 mt-6'}>
         <FooterForm/>
       </div>
     </div>
