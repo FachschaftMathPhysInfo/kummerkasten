c5594dedfa9598287e164a4ab7ac49be3bde386a
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-10-06 15:51:48 +0200

    add: logo

--- frontend/app/(kummer-form)/page.tsx
+++ frontend/app/(kummer-form)/page.tsx
@@ -5,8 +5,11 @@ import FAQSection from './form-faq';
 import FormUi from './form-ui';
 import Image from 'next/image';
 import ThemeSwitch from "@/components/theme-switch";
+import {useTheme} from "next-themes";
 
 export default function KummerkastenPage() {
+  const {resolvedTheme} = useTheme();
+
   return (
     <main className={'min-h-screen w-full relative'}>
       <div
@@ -15,13 +18,14 @@ export default function KummerkastenPage() {
           className={'absolute top-1 left-1 mt-2 ml-2 lg:mt-4 lg:ml-4'}
         />
 
-        <div className="flex items-center">
+        <div className="flex items-center gap-5">
           <Image
-            src="/logo.png"
+            suppressHydrationWarning
+            src={resolvedTheme === "dark" ? "/logo_dark.svg" :"/logo_light.svg"}
             alt="Kummerkasten Logo"
-            width={1024}
-            height={1024}
-            className="w-16 h-16 sm:w-16 sm:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 rounded-full mr-4"
+            width={512}
+            height={512}
+            className="w-16 h-16 sm:w-16 sm:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 mr-4"
           />
           <h1
             className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold tracking-tight text-foreground"
